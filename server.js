e7c680ca0fe9963db407b6bee7053f07a80471ba
Pranjal Gupta <pranjalgupta@Pranjals-MacBook-Air.local>
2025-09-24 21:59:28 +0530

    changes

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello from the Node.js backend!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
